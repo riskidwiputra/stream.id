f675a4a9aafddbf5f3d4c55cdab9c58df24e608e
riskidwiputra1250@gmail.com <riskidwiputra1250@gmail.com>
2020-01-29 17:48:25 +0700

    perbaikan config

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -10,15 +10,15 @@ class Config
 		date_default_timezone_set('Asia/Jakarta'); 
 		ini_set( 'display_errors', 0 );    
 
-		// define('DB_HOST', 'localhost');
-		// define('DB_USER', 'u8127881_gaming');
-		// define('DB_PASS', 'gamingstreamuniverse');
-		// define('DB_NAME', 'u8127881_gaming');
-
 		define('DB_HOST', 'localhost');
-		define('DB_USER', 'root');
-		define('DB_PASS', '');
-		define('DB_NAME', 'stream');
+		define('DB_USER', 'u8127881_gaming');
+		define('DB_PASS', 'gamingstreamuniverse');
+		define('DB_NAME', 'u8127881_gaming');
+
+		// define('DB_HOST', 'localhost');
+		// define('DB_USER', 'root');
+		// define('DB_PASS', '');
+		// define('DB_NAME', 'stream');
  
 		define('CONTROLLER', 'Home');
 		define('METHOD', 'error');	
