93e9cdca843fd1e3856520bc12bf1e4f3e5f2d90
riskidwiputra1250@gmail.com <riskidwiputra1250@gmail.com>
2020-01-29 19:13:56 +0700

    perbaian

--- app/Controllers/TournamentController.php
+++ app/Controllers/TournamentController.php
@@ -29,29 +29,29 @@
 			$data['group']	= $this->db->query("SELECT * FROM tournament_group WHERE id_group = '$id_group2'");
 			$data['group']	= $this->db->resultSet();
 			
-			
-			$data['round32'] = $this->db->query(
-				"SELECT
-					team_1.nama_team AS team_1,
-					team_2.nama_team AS team_2,
-					team_1.logo_team AS logo_1,
-					team_2.logo_team AS logo_2,
-					team_1.nama_kota AS nama_kota_1,
-					team_2.nama_kota AS nama_kota_2,
+			var_dump($id_tournament);
+			// $data['round32'] = $this->db->query(
+			// 	"SELECT
+			// 		team_1.nama_team AS team_1,
+			// 		team_2.nama_team AS team_2,
+			// 		team_1.logo_team AS logo_1,
+			// 		team_2.logo_team AS logo_2,
+			// 		team_1.nama_kota AS nama_kota_1,
+			// 		team_2.nama_kota AS nama_kota_2,
 					
 
-					tournament_round32.team1,
-					tournament_round32.team2,
-					tournament_round32.id_round32,
-					tournament_round32.winner,
-					tournament_round32.date
+			// 		tournament_round32.team1,
+			// 		tournament_round32.team2,
+			// 		tournament_round32.id_round32,
+			// 		tournament_round32.winner,
+			// 		tournament_round32.date
 	
-				FROM tournament_round32
-				JOIN tournament_group AS team_1 ON tournament_round32.team1=team_1.id_team
-				JOIN tournament_group AS team_2 ON tournament_round32.team2=team_2.id_team
-				WHERE id_tournament = '$id_tournament'
-				");
-			$data['round32'] = $this->db->resultSet();
+			// 	FROM tournament_round32
+			// 	JOIN tournament_group AS team_1 ON tournament_round32.team1=team_1.id_team
+			// 	JOIN tournament_group AS team_2 ON tournament_round32.team2=team_2.id_team
+			// 	WHERE id_tournament = '$id_tournament'
+			// 	");
+			// $data['round32'] = $this->db->resultSet();
 			// $data['round16'] = $this->db->query(
 			// 	"SELECT
 			// 	c.nama_team AS team_1,
