8cb42a361170fca680e9da282a2bd65171127c4c
Luthfi Asri <37830437+luthfiasri@users.noreply.github.com>
2020-02-27 16:13:22 +0700

    AAHHH

--- app/Controllers/ListTeamController.php
+++ app/Controllers/ListTeamController.php
@@ -8,8 +8,12 @@
 		
 
 		public function listteam()
-		{ 
-			$this->view('landing/template/header');
+		{ 	
+			$data['populared'] = $this->db->query("SELECT * FROM news_game ORDER by views DESC LIMIT 2 ");
+			$data['populared'] = $this->db->resultSet();
+			$data['game-list'] = $this->db->table('game_list')->all();
+			$data['game-list']	= $this->db->resultSet();
+			$this->view('landing/template/header',$data);
 			$this->view('landing/team/listteam');	
 			$this->view('landing/template/footer');			
 			// var_dump($_SERVER);die;
