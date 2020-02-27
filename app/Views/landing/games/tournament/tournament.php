fefb15d59ad754de875d04f1ea226e6a8b6caef6
Luthfi Asri <37830437+luthfiasri@users.noreply.github.com>
2020-02-27 17:18:20 +0700

    ugh

--- app/Views/landing/games/tournament/tournament.php
+++ app/Views/landing/games/tournament/tournament.php
@@ -56,15 +56,19 @@
 								<div class="countdown-counter match-preview__action--ticket text-center" data-date="<?= $waktu ?>"></div>	
                                     <?php if(strtotime(date('d-m-Y H:i')) >= strtotime($rows['date'])){ ?>
                                         <?php if ($rows['status'] == "pending") {?>
-                                            <a  class="btn btn-primary-inverse text-center" data-toggle="tooltip" data-placement="top" title="ONCOMING" ">View</a>
+                                            <a  class="btn btn-primary-inverse text-center mr-2" data-toggle="tooltip" data-placement="top" title="ONCOMING">View</a>
+                                            <a class="btn btn-info btn-outline text-center text-white" data-target="#modaljoin" data-toggle="modal" data-placement="top" title="ONCOMING">Join</a>
                                         <?php } else if ($rows['status'] == "starting"){?>
-                                            <a href="<?= BASEURL ?>/tournament/<?= $rows['url'] ?>"class="btn btn-primary-inverse text-center">View</a>
+                                            <a href="<?= BASEURL ?>/tournament/<?= $rows['url'] ?>"class="btn btn-primary-inverse text-center mr-2">View</a>
+                                            <a class="btn btn-info btn-outline text-center text-white" data-target="#modaljoin" data-toggle="modal" data-placement="top" title="ONCOMING">Join</a>
                                         <?php }else if ($rows['status'] == "complete") {?>
-                                            <a href="<?= BASEURL ?>/tournament/<?= $rows['url'] ?>"class="btn btn-primary-inverse text-center">View</a>
+                                            <a href="<?= BASEURL ?>/tournament/<?= $rows['url'] ?>"class="btn btn-primary-inverse text-center mr-2">View</a>
+                                            <a class="btn btn-info btn-outline text-center text-white" data-target="#modaljoin" data-toggle="modal" data-placement="top" title="ONCOMING">Join</a>
                                         <?php } ?>
                                 
                                     <?php }else{?>  
-                                    <a class="btn btn-primary-inverse text-center" data-toggle="tooltip" data-placement="top" title="ONCOMING">View</a>
+                                    <a class="btn btn-primary-inverse text-center mr-2" data-toggle="tooltip" data-placement="top" title="ONCOMING">View</a>
+                                    <a class="btn btn-info btn-outline text-center text-white" data-target="#modaljoin" data-toggle="modal" data-placement="top" title="ONCOMING">Join</a>
                                     <?php } ?>
                                 </div>
                     
@@ -76,6 +80,40 @@
                     </aside>
                 </div>
             <?php } ?>
+        </div>
+        <div class="modal fade" id="modaljoin" tabindex="-1" role="dialog" aria-labelledby="modaljoinLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content border-0 rounded-circle">
+                    <div class="modal-header border-success" style="background-color: #4B3B60;">
+                        <h1>Create A Team</h1>
+                    </div>	
+                    <div class="modal-body border-success" style="background-color: #4B3B60;">
+                        <div class="form-group form-group--sm">
+                            <label for="billing_address_1">Team Name <abbr class="required" title="required">*</abbr></label>
+                            <input type="text" name="billing_address_1" id="billing_address_1" class="form-control" disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="register-name">Payment Registration <abbr class="required" title="required">*</abbr></label>
+                            <input type="number" class="form-control">
+                        </div>
+                        <label for="register-name">Payment Method <abbr class="required" title="required">*</abbr></label>
+                        <div class="form-group">
+                        		<label class="radio radio-inline mr-2">
+								<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" checked> Stream Cash
+								<span class="radio-indicator"></span>
+							</label>
+							<label class="radio radio-inline mr-2">
+								<input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> Stream Point
+								<span class="radio-indicator"></span>
+							</label>
+						</div>
+                    </div>
+                    <div class="modal-footer border-success" style="background-color: #4B3B60;">
+                        <a href="#" class="btn btn-info btn-outline">Join</a>
+                        <a href="#" class="btn btn-default" data-dismiss="modal">Close</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
