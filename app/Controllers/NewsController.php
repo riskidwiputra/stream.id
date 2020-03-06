daa8bbbc480bceba528b78e72b807030b382a860
Mhd Ramadhan Arvin <38186144+mhdramadhanarvin@users.noreply.github.com>
2020-03-06 17:07:21 +0700

    Update NewsController.php

--- app/Controllers/NewsController.php
+++ app/Controllers/NewsController.php
@@ -9,6 +9,20 @@
 		{
 			parent::__construct();
 
+			if (Session::check('users')) {
+				$this->Users = $this->db->query('
+					SELECT * FROM users 
+					JOIN users_docs
+					ON users.user_id = users_docs.user_id
+					JOIN balance_users
+					ON users.user_id = balance_users.users_id
+					WHERE users.user_id = "'.Session::get("users").'"
+					');
+				$this->Users	= $this->db->single();  
+			} else {
+				$this->Users = '';
+			}
+
 		}
 		
 		public function Index()
@@ -25,17 +39,9 @@
 			$jumlahData 			= $this->db->single();
 			$limit					= 6;
 			$data['jumlahHalaman'] 	= ceil($jumlahData['jumlah'] / $limit);
-			$data['game-list'] = $this->db->table('game_list')->all();
-			$data['game-list']	= $this->db->resultSet();
-			$data['users'] = $this->db->query('
-				SELECT * FROM users 
-				JOIN users_docs
-				ON users.user_id = users_docs.user_id
-				JOIN balance_users
-				ON users.user_id = balance_users.users_id
-				WHERE users.user_id = "'.Session::get("users").'"
-				');
-			$data['users']	= $this->db->single();  
+			$data['game-list'] 		= $this->db->table('game_list')->all();
+			$data['game-list']		= $this->db->resultSet();
+			$data['users'] 			= $this->Users;
 			$data['content'] 		= $this->model('News_Model')->select();
 			$this->view('landing/template/header', $data);
 			$this->view('news/news', $data);	
@@ -54,17 +60,9 @@
 			$data['commented'] 		= $this->db->resultSet();
 			$jumlahData 			= $this->db->query("SELECT COUNT(*) AS jumlah FROM news_game");
 			$jumlahData 			= $this->db->single();
-			$data['users'] = $this->db->query('
-				SELECT * FROM users 
-				JOIN users_docs
-				ON users.user_id = users_docs.user_id
-				JOIN balance_users
-				ON users.user_id = balance_users.users_id
-				WHERE users.user_id = "'.Session::get("users").'"
-				');
-			$data['users']	= $this->db->single();  
-			$data['game-list'] = $this->db->table('game_list')->all();
-			$data['game-list']	= $this->db->resultSet();
+			$data['users'] 			= $this->Users;
+			$data['game-list'] 		= $this->db->table('game_list')->all();
+			$data['game-list']		= $this->db->resultSet();
 			// batas news yang ingin di tampilkan 
 			$limit					= 6;
 			// mengambil angka page 
@@ -82,27 +80,20 @@
 
 		public function SingleNews($id)
 		{ 	
-			$data['single'] = $this->db->table('news_game')->where('url', $id);
-			$data['single'] = $this->db->single();	
-			$data['comment'] = $this->db->table('komentar')->countRows('id_news_game', $data['single']['id_news_game']);
-			$data['newest'] = $this->db->query("SELECT * FROM news_game ORDER by id_news_game DESC LIMIT 5 ");
-			$data['newest'] = $this->db->resultSet();
-			$data['popular'] = $this->db->query("SELECT * FROM news_game ORDER by views DESC LIMIT 5 ");
-			$data['popular'] = $this->db->resultSet();
-			$data['populared'] = $this->db->query("SELECT * FROM news_game ORDER by views DESC LIMIT 2 ");
-			$data['populared'] = $this->db->resultSet();
-			$data['commented'] = $this->db->query("SELECT * FROM news_game ORDER by komentar DESC LIMIT 5 ");
-			$data['commented'] = $this->db->resultSet();
-			$data['content'] = $this->db->table('news_game')->where('id_news_game', $data['single']['id_news_game']);
-			$data['content'] = $this->db->single();
-			$data['users'] = $this->db->query('
-				SELECT * FROM users 
-				JOIN users_docs
-				ON users.user_id = users_docs.user_id
-				JOIN balance_users
-				ON users.user_id = balance_users.users_id
-				WHERE users.user_id = "'.Session::get("users").'"
-			'); 
+			$data['single'] 		= $this->db->table('news_game')->where('url', $id);
+			$data['single'] 		= $this->db->single();	
+			$data['comment'] 		= $this->db->table('komentar')->countRows('id_news_game', $data['single']['id_news_game']);
+			$data['newest'] 		= $this->db->query("SELECT * FROM news_game ORDER by id_news_game DESC LIMIT 5 ");
+			$data['newest'] 		= $this->db->resultSet();
+			$data['popular'] 		= $this->db->query("SELECT * FROM news_game ORDER by views DESC LIMIT 5 ");
+			$data['popular'] 		= $this->db->resultSet();
+			$data['populared']	 	= $this->db->query("SELECT * FROM news_game ORDER by views DESC LIMIT 2 ");
+			$data['populared'] 		= $this->db->resultSet();
+			$data['commented'] 		= $this->db->query("SELECT * FROM news_game ORDER by komentar DESC LIMIT 5 ");
+			$data['commented']	 	= $this->db->resultSet();
+			$data['content'] 		= $this->db->table('news_game')->where('id_news_game', $data['single']['id_news_game']);
+			$data['content'] 		= $this->db->single();
+			$data['users'] 			= $this->Users;
 
 			$tag = explode(',',$data['single']['tag']);  
 			$data['related'] = $this->db->query('
@@ -131,8 +122,7 @@
 			} 
 			// var_dump($data['label']);die;
 			$data['author'] = $this->db->table('data_management')->where('stream_id', $data['content']['penulis']);
-			$data['author'] = $data['author']['fullname'];
-			$data['users']	= $this->db->single();  
+			$data['author'] = $data['author']['fullname']; 
 			$data['game-list'] = $this->db->table('game_list')->all();
 			$data['game-list']	= $this->db->resultSet();
 			$view = [
